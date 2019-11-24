88da2119b647d9ffa7e63a5a14802623092881e4
Ric <rcalixtos@gmail.com>
2019-11-24 16:08:04 -0300

    Updates

---
+++ precache-manifest.51b6e7c311601adc93f2f50ffc8fa042.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2127fff979891d8a7295d846406e2ba4",
+    "url": "/random-androids/index.html"
+  },
+  {
+    "revision": "92f45498d07c0ac8d8e2",
+    "url": "/random-androids/static/css/main.3cbf8606.chunk.css"
+  },
+  {
+    "revision": "6d84a152a17fa7e7c311",
+    "url": "/random-androids/static/js/2.8a38d52c.chunk.js"
+  },
+  {
+    "revision": "92f45498d07c0ac8d8e2",
+    "url": "/random-androids/static/js/main.d73ad266.chunk.js"
+  },
+  {
+    "revision": "132e9fd37be34b6ceaf5",
+    "url": "/random-androids/static/js/runtime-main.6887aa73.js"
+  }
+]);
